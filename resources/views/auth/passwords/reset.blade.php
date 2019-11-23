95da5886c10446dac88acb671fa25a7ec30c2be9
vagrant <vagrant@homestead>
2019-11-23 11:48:55 +0000

    找回密码成功消息提示

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -4,13 +4,13 @@
 
   @include('layouts._lg_other_header')
 
-    <div class="container">
+    <div class="container text-white-50">
         <div class="row justify-content-center">
             <div class="col-md-8">
 
 
-                <div class="card">
-                    <div class="card-header">{{ __('Reset Password') }}</div>
+                <div class="card login">
+                    <div class="card-header"><h4>{{ __('Reset Password') }}</h4></div>
 
                     <div class="card-body">
                         <form method="POST" action="{{ route('password.update') }}">
@@ -20,7 +20,7 @@
 
                             <div class="form-group row">
                                 <label for="email"
-                                       class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                                       class="col-md-4 col-form-label text-md-right"><h5>{{ __('E-Mail Address') }}</h5></label>
 
                                 <div class="col-md-6">
                                     <input id="email" type="email"
@@ -37,7 +37,7 @@
 
                             <div class="form-group row">
                                 <label for="password"
-                                       class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                                       class="col-md-4 col-form-label text-md-right"><h5>{{ __('Password') }}</h5></label>
 
                                 <div class="col-md-6">
                                     <input id="password" type="password"
@@ -54,7 +54,7 @@
 
                             <div class="form-group row">
                                 <label for="password-confirm"
-                                       class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                                       class="col-md-4 col-form-label text-md-right"><h5>{{ __('Confirm Password') }}</h5></label>
 
                                 <div class="col-md-6">
                                     <input id="password-confirm" type="password" class="form-control"
@@ -64,7 +64,7 @@
 
                             <div class="form-group row mb-0">
                                 <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
+                                    <button type="submit" class="btn btn-secondary login text-white-50">
                                         {{ __('Reset Password') }}
                                     </button>
                                 </div>
